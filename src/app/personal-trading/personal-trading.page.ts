37799d3f2d4977f95cfee9b8191b21cc99997fcb
Nayan Khokhar <2000nayankhokhar@gmail.com>
2024-10-19 00:53:40 +0530

    Shareholding-Pattern Completed

--- src/app/personal-trading/personal-trading.page.ts
+++ src/app/personal-trading/personal-trading.page.ts
@@ -15,7 +15,7 @@ export class PersonalTradingPage implements OnInit {
   insiderTradingFiltered: any = [];
 
   private shareholdingPatterns: any = [];    // CF-Shareholding-Pattern-equities
-  private regulation29SAST: any = [];        // CF-SAST- Reg29
+  private SASTReg29: any = [];        // CF-SAST- Reg29
   private pledgedData: any = [];             // CF-SAST-Pledged-Data
 
 
@@ -61,7 +61,7 @@ export class PersonalTradingPage implements OnInit {
         } else if (fileName.includes('CF-Shareholding-Pattern-equities')) {
           this.shareholdingPatterns = result.data;
         } else if (fileName.includes('CF-SAST- Reg29')) {
-          this.regulation29SAST = result.data;
+          this.SASTReg29 = result.data;
         } else if (fileName.includes('CF-SAST-Pledged-Data')) {
           this.pledgedData = result.data;
         }
@@ -71,26 +71,52 @@ export class PersonalTradingPage implements OnInit {
 
   showAllData() {
     console.log("InsiderTrading: ", this.insiderTrading);
-    // console.log("ShareholdingPatterns: ", this.shareholdingPatterns);
-    // console.log("Regulation29SAST: ", this.regulation29SAST);
+    console.log("ShareholdingPatterns: ", this.shareholdingPatterns);
+    console.log("SASTReg29: ", this.SASTReg29);
     // console.log("PledgedData: ", this.pledgedData);
 
-    const marketPurchaseIsiderTrading = _.filter(this.insiderTrading, item => ((item['CATEGORY OF PERSON \n'] == "Promoters" || item['CATEGORY OF PERSON \n'] == "Promoter Group") && item['MODE OF ACQUISITION \n'] == "Market Purchase")
+    const marketPurchaseIsiderTrading = _.filter(this.insiderTrading, item => ((item['CATEGORY OF PERSON \n'] == "Promoters" || item['CATEGORY OF PERSON \n'] == "Promoter Group") && (item['MODE OF ACQUISITION \n'] == "Market Purchase" || item['MODE OF ACQUISITION \n'] == "Market Sale"))
     );
     console.log("MarketPurchaseIsiderTrading: ", marketPurchaseIsiderTrading);
 
+    const removeSellIsiderTrading = _.chain(marketPurchaseIsiderTrading)
+      .groupBy('COMPANY \n')
+      .filter(group => {
+        return !_.some(group, { 'MODE OF ACQUISITION \n': 'Market Sale' });
+      }).flatten().value();
+    console.log("RemoveSellIsiderTrading: ", removeSellIsiderTrading);
 
-    this.insiderTradingFiltered = _(marketPurchaseIsiderTrading).groupBy('COMPANY \n').map((entries, company) => {
-      const totalValue = _.sumBy(entries, 'VALUE OF SECURITY (ACQUIRED/DISPLOSED) \n');
+
+
+    const insiderTradingFilteredAvg = _(removeSellIsiderTrading).groupBy('COMPANY \n').map((entries, company) => {
+      const totalPurchase = _.sumBy(entries, 'VALUE OF SECURITY (ACQUIRED/DISPLOSED) \n');
+      const totalShare = _.sumBy(entries, 'NO. OF SECURITIES (ACQUIRED/DISPLOSED) \n');
       return {
         symbol: entries[0]['SYMBOL \n'],
         COMPANY: company,
-        "VALUE OF SECURITY (ACQUIRED/DISPLOSED) \n": totalValue,
+        "VALUE OF SECURITY (ACQUIRED/DISPLOSED) \n": totalPurchase,
+        "NO. OF SECURITIES (ACQUIRED/DISPLOSED) \n": totalShare,
+
+        Average_Purchase: (totalPurchase / totalShare)
       };
     }).orderBy('VALUE OF SECURITY (ACQUIRED/DISPLOSED) \n', 'desc').value();
+    console.log("InsiderTradingFilteredAvg: ", insiderTradingFilteredAvg);
 
-    console.log("InsiderTradingFilter: ", this.insiderTradingFiltered);
-    console.log("Sum: ", (1985000000+503255000+50325000+385425000+373950000));
-    
+    this.insiderTradingFiltered = _.filter(insiderTradingFilteredAvg, (insider: any) => {
+      const matchingShareholder = _.find(this.shareholdingPatterns, { COMPANY: insider.COMPANY });
+
+      if (matchingShareholder) {
+        if (matchingShareholder['PROMOTER & PROMOTER GROUP (A)'] > 50) {
+          insider.inShareHolder = true;
+          return true; // Keep this entry
+        } else {
+          return false; // Remove this entry
+        }
+      } else {
+        insider.inShareHolder = false;
+        return true; // Remove this entry
+      }
+    });
+    console.log("InsiderTradingFiltered: ", this.insiderTradingFiltered);
   }
 }
