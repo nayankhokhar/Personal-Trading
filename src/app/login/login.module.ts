5cb759a60f9e3d4fa1ffb5259403e5b9fb11a43b
Nayan Khokhar <2000nayankhokhar@gmail.com>
2024-10-29 22:25:33 +0530

    Login page added

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -6,12 +6,14 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [LoginPage]
 })
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  constructor() { }
+  loginForm!: FormGroup;
+  submitted = false;
+
+  constructor(
+    private elRef: ElementRef,
+    private renderer: Renderer2,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ["", [Validators.required]],
+      password: ["", [Validators.required]]
+    });
     // localStorage.setItem("username", "test");
     // localStorage.setItem("password", "test");
+  }
+
+  ngAfterViewInit() {
+    const inputElements = this.elRef.nativeElement.querySelectorAll('ion-input');
+    inputElements.forEach((input: any) => {
+      this.renderer.setAttribute(input, 'mode', 'md');
+    });
+  }
+
+  onLogin() {
+    this.submitted = true;
+    if (this.loginForm.valid) {
+      // this.loginForm.value;
+      // localStorage.setItem("username", "test");
+      // localStorage.setItem("password", "test");
+    }
   }
 }
